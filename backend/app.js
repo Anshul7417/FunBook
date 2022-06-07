fe45a4a861f1a0b634a5a52c3d7309573abd8ab7
Anshul Tripathi <tripathianshul06@gmail.com>
2022-06-08 00:46:17 +0530

    Schema Created

--- backend/app.js
+++ backend/app.js
@@ -3,4 +3,16 @@ const app =express();
 
 require("dotenv").config({path:"backend\\config\\config.env"});
 
+// using middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//importing Routes
+
+const post = require ("./routes/post");
+
+//using Routes
+
+app.use("/api/v1", post)
+
 module.exports=app;
---
+++ backend/controllers/post.js
@@ -0,0 +1,22 @@
+const post = require("../models/Post");
+createPost = async (req,res) => {
+    try {
+        
+        const newPostData = {
+            caption:req.body.caption,
+            image:{
+                public_id:"req.body.public_id",
+                url:"req.body.url"
+            },
+        }
+
+
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+module.exports=createPost;
