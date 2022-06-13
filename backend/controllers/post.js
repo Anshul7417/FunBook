43596887aa9b11421984cec59790f045902f30e5
Anshul Tripathi <tripathianshul06@gmail.com>
2022-06-13 22:07:45 +0530

    Backend Half done

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -33,13 +33,60 @@ exports.createPost = async (req,res) => {
     }
 }
 
+exports.deletePost = async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+  
+      if (!post) {
+        return res.status(404).json({
+          success: false,
+          message: "Post not found",
+        });
+      }
+  
+      if (post.owner.toString() !== req.user._id.toString()) {    // checking owner==post deleting user
+        return res.status(401).json({
+          success: false,
+          message: "Unauthorized",
+        });
+      }
+  
+    //   await cloudinary.v2.uploader.destroy(post.image.public_id);
+  
+      await post.remove();
+  
+      const user = await User.findById(req.user._id);
+  
+      const index = user.posts.indexOf(req.params.id);
+      user.posts.splice(index, 1);
+  
+      await user.save();
+  
+      res.status(200).json({
+        success: true,
+        message: "Post deleted",
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  };
+
 
 exports.likeAndUnlikePost = async (req,res)=>{
     try {
 
         const post = await Post.findById(req.params.id);
 
-       if(post.likes.includes(req.user._id)) {
+        if(!post){
+            return res.status(404).json({
+                success:false,
+                message:"Post not Found",
+            })
+        }
+       if(post.likes.includes(req.user._id)) {    //unlike
             const index = post.likes.indexof(req.user._id);
             post.likes.splice(index,1);
 
@@ -51,7 +98,20 @@ exports.likeAndUnlikePost = async (req,res)=>{
             })
        }
 
-       post.likes.push(req.user._id)
+       else{
+        post.likes.push(req.user._id);    // like
+
+        await post.save();
+
+        return res.status(200).json({
+            success:true,
+            message:" Post Liked",
+        })
+
+
+       }
+
+       
         
     } catch (error) {
         res.status(500).json({
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -19,7 +19,7 @@ exports.register = async (req, res) => {    // User Registeration
         const options ={
             expires: new Date(Date.now()+90*24*60*60*1000),
             httpOnly:true ,
-        };
+        }; 
 
         res.status(200).cookie("token",token,options).json({
             success:true,
@@ -79,4 +79,50 @@ exports.register = async (req, res) => {    // User Registeration
                 message: error.message,
             })
         }
-    }
+    };
+    
+    exports.followUser = async (req, res) => {
+      try {
+        const userToFollow = await User.findById(req.params.id); 
+        const loggedInUser = await User.findById(req.user._id);
+    
+        if (!userToFollow) {
+          return res.status(404).json({
+            success: false,
+            message: "User not found",
+          });
+        }
+    
+        if (loggedInUser.following.includes(userToFollow._id)) {      //unfollowing 
+          const indexfollowing = loggedInUser.following.indexOf(userToFollow._id);
+          const indexfollowers = userToFollow.followers.indexOf(loggedInUser._id);
+    
+          loggedInUser.following.splice(indexfollowing, 1);
+          userToFollow.followers.splice(indexfollowers, 1);
+    
+          await loggedInUser.save();
+          await userToFollow.save();
+    
+          res.status(200).json({
+            success: true,
+            message: "User Unfollowed",
+          });
+        } else {                  //following
+          loggedInUser.following.push(userToFollow._id);
+          userToFollow.followers.push(loggedInUser._id);
+    
+          await loggedInUser.save();
+          await userToFollow.save();
+    
+          res.status(200).json({
+            success: true,
+            message: "User followed",
+          });
+        }
+      } catch (error) {
+        res.status(500).json({
+          success: false,
+          message: error.message,
+        });
+      }
+    };
